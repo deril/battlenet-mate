182380fc1212ae2605fc90e61ab791bfaac4501d
Kostya Dubinin <dubininkostya@gmail.com>
2013-10-29 19:50:06 +0200

    Add dummy functionality to Home#index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @battletag_name = 'Idmon'
+    @battletag_code = 2666
+    @host = 'http://eu.battle.net'
   end
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,5 @@
 module HomeHelper
+  def d3_api_url(host, battletag_name, battletag_code)
+    "#{host}/api/d3/profile/#{battletag_name}-#{battletag_code}/"
+  end
 end
