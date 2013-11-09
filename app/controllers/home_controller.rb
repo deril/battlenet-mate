99eef6db1f7b5a7941fcaba47c0060fa77f15d9a
Kostya Dubinin <dubininkostya@gmail.com>
2013-11-09 21:21:21 +0200

    Convert instance variables into just variables

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,15 +2,15 @@ class HomeController < ApplicationController
   def wow
     api = Battlenet.new :eu
     @character = api.character('Chamber of Aspects', 'Idmon', fields: 'titles')
-    @races = api.character_races['races']
-    @classes = api.character_classes['classes']
+    races = api.character_races['races']
+    classes = api.character_classes['classes']
 
     title_pattern = @character['titles'].find { |title| title['selected'] }.try(:[], 'name')
     @full_name = title_pattern % @character['name'] if title_pattern
     @full_name ||= @character['name']
 
-    @race_name = @races.find { |race| race['id'] == @character['race']}['name']
-    @class_name = @classes.find { |c| c['id'] == @character['class']}['name']
+    @race_name = races.find { |race| race['id'] == @character['race']}['name']
+    @class_name = classes.find { |c| c['id'] == @character['class']}['name']
   end
 
   def diablo
